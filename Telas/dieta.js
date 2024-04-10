4073cc8912cf4708701c47c92d26aef7fb743347
gugapototski <gugapototski@live.com>
2024-04-09 23:22:59 -0300

    add telas de dieta Gabriel

--- Telas/dieta.js
+++ Telas/dieta.js
@@ -1,10 +1,49 @@
-import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
+import arrowDown from '../Imagens/flechabaixo.png';
+import arrowUp from '../Imagens/flechacima.png';
+import clockIcon from '../Imagens/relogio.png';
 
-const Dieta = () => {
+const DietScreen = () => {
+  const [selectedMealId, setSelectedMealId] = useState(null);
+
+  const handlePress = (mealId) => {
+    if (selectedMealId === mealId) {
+      setSelectedMealId(null);
+    } else {
+      setSelectedMealId(mealId);
+    }
+  };
+
+  const meals = [
+    { id: 1, name: 'Refeição 1                                    9:15', foods: ['Ovos', 'Aveia', 'Frutas'] },
+    { id: 2, name: 'Refeição 2                                  12:00', foods: ['Iogurte', 'Frutas', 'Castanhas'] },
+    { id: 3, name: 'Refeição 3                                  16:00', foods: ['Arroz', 'Feijão', 'Frango'] },
+    { id: 4, name: 'Refeição 4                                  18:00', foods: ['Sanduíche', 'Frutas', 'Chá'] },
+    { id: 5, name: 'Refeição 5                                  22:00', foods: ['Legumes à vontade', 'Frango: 120g', 'Arroz: 250g'] },
+  ];
+
+
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Tela de Dieta</Text>
+      {meals.map((meal) => (
+        <View key={meal.id} style={[styles.mealContainer, styles.greenBorder]}>
+          <TouchableOpacity onPress={() => handlePress(meal.id)} style={styles.row}>
+            <View style={styles.row}>
+              <Image source={clockIcon} style={styles.clockIcon} />
+              <Text style={[styles.title]}>{meal.name}</Text>
+            </View>
+            <Image source={selectedMealId === meal.id ? arrowUp : arrowDown} style={styles.arrowIcon} />
+          </TouchableOpacity>
+          {selectedMealId === meal.id && (
+            <View style={styles.optionsContainer}>
+              {meal.foods.map((food, index) => (
+                <Text key={index} style={[styles.option]}>{food}</Text>
+              ))}
+            </View>
+          )}
+        </View>
+      ))}
     </View>
   );
 };
@@ -12,14 +51,52 @@ const Dieta = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "black",
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'black',
   },
-  text: {
-    fontSize: 20,
-    color: "white",
+  mealContainer: {
+    marginBottom: 30,
+    width: '75%',
+    padding: 20,
+    borderWidth: 1,
+    borderColor: '#3EA519',
+    borderRadius: 15,
+  },
+  row: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  title: {
+    fontSize: 17,
+    fontWeight: 'bold',
+    color: 'white',
+    marginBottom: 1,
+  },
+  subtitle: {
+    fontSize: 17,
+    color: 'green',
+    marginBottom: 5,
+  },
+  optionsContainer: {
+    backgroundColor: 'black',
+    padding: 10,
+  },
+  option: {
+    fontSize: 17,
+    color: 'white',
+    marginBottom: 5,
+  },
+  arrowIcon: {
+    width: 20,
+    height: 40,
+  },
+  clockIcon: {
+    width: 20,
+    height: 20,
+    marginRight: 10,
   },
 });
 
-export default Dieta;
+export default DietScreen;
--- Telas/dietaPersonal.js
+++ Telas/dietaPersonal.js
@@ -3,15 +3,24 @@ import { View, Text, StyleSheet, TouchableOpacity, Image } from 'react-native';
 import arrowDown from '../Imagens/flechabaixo.png';
 import arrowUp from '../Imagens/flechacima.png';
 import clockIcon from '../Imagens/relogio.png';
+import editIcon from '../Imagens/editor.png';
 
-const DietScreen = () => {
+const DietaPersonal = () => {
   const [selectedMealId, setSelectedMealId] = useState(null);
 
-  const handlePress = (mealId) => {
+  const handleClockPress = (mealId) => {
     if (selectedMealId === mealId) {
       setSelectedMealId(null);
     } else {
       setSelectedMealId(mealId);
+    }
+  };
+
+  const handleEditPress = (mealId) => {
+    if (selectedMealId !== mealId) {
+      setSelectedMealId(mealId);
+    } else {
+      setSelectedMealId(null);
     }
   };
 
@@ -23,22 +32,26 @@ const DietScreen = () => {
     { id: 5, name: 'Refeição 5                                  22:00', foods: ['Legumes à vontade', 'Frango: 120g', 'Arroz: 250g'] },
   ];
 
-
   return (
     <View style={styles.container}>
       {meals.map((meal) => (
         <View key={meal.id} style={[styles.mealContainer, styles.greenBorder]}>
-          <TouchableOpacity onPress={() => handlePress(meal.id)} style={styles.row}>
-            <View style={styles.row}>
+          <View style={styles.row}>
+            <TouchableOpacity onPress={() => handleClockPress(meal.id)} style={styles.clockContainer}>
               <Image source={clockIcon} style={styles.clockIcon} />
-              <Text style={[styles.title]}>{meal.name}</Text>
-            </View>
-            <Image source={selectedMealId === meal.id ? arrowUp : arrowDown} style={styles.arrowIcon} />
-          </TouchableOpacity>
+              <Text style={styles.title}>{meal.name}</Text>
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleEditPress(meal.id)} style={styles.editContainer}>
+              <Image source={editIcon} style={styles.editIcon} />
+            </TouchableOpacity>
+            <TouchableOpacity onPress={() => handleClockPress(meal.id)} style={styles.arrowContainer}>
+              <Image source={selectedMealId === meal.id ? arrowUp : arrowDown} style={styles.arrowIcon} />
+            </TouchableOpacity>
+          </View>
           {selectedMealId === meal.id && (
             <View style={styles.optionsContainer}>
               {meal.foods.map((food, index) => (
-                <Text key={index} style={[styles.option]}>{food}</Text>
+                <Text key={index} style={styles.option}>{food}</Text>
               ))}
             </View>
           )}
@@ -68,16 +81,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-between',
   },
+  clockContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
   title: {
     fontSize: 17,
     fontWeight: 'bold',
     color: 'white',
     marginBottom: 1,
-  },
-  subtitle: {
-    fontSize: 17,
-    color: 'green',
-    marginBottom: 5,
   },
   optionsContainer: {
     backgroundColor: 'black',
@@ -91,12 +103,27 @@ const styles = StyleSheet.create({
   arrowIcon: {
     width: 20,
     height: 40,
+    marginRight: 5,
+  },
+  editIcon: {
+    width: 25,
+    height: 25,
   },
   clockIcon: {
     width: 20,
     height: 20,
     marginRight: 10,
   },
+  arrowContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  editContainer: {
+    position: 'absolute',
+    right: -60, 
+    top: '50%',
+    transform: [{ translateY: -10 }],
+  },
 });
 
-export default DietScreen;
+export default DietaPersonal;
